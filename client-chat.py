f6812256ccf39afa2f60e34cef52e740cf997cc4
uttham <uttham1@gmail.com>
2021-11-19 13:42:21 +0530

    update 1

--- client-chat.py
+++ client-chat.py
@@ -8,11 +8,7 @@ from colorama import Fore, init, Back
 init()
 
 # set the available colors
-colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
-    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
-    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
-    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
-]
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW]
 
 # choose a random color for the client
 client_color = random.choice(colors)
